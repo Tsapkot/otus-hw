2ee7344613e3b901c2bab7967cd045087b74bbd8
vtsapko <vtsapko@diasoft.ru>
2024-11-24 18:24:12 +0400

    hw3

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,42 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
+import (
+	"sort"
+	"strings"
+)
+
+type Freq struct {
+	Name    string
+	Counter int
+}
+
+func Top10(s string) []string {
 	// Place your code here.
-	return nil
+	countMap := make(map[string]int)
+
+	freqSlice := make([]Freq, 0)
+
+	for _, s := range strings.Fields(s) {
+		value := countMap[s]
+		countMap[s] = value + 1
+	}
+	for key, value := range countMap {
+		freqSlice = append(freqSlice, Freq{key, value})
+	}
+
+	sort.Slice(freqSlice, func(i, j int) bool {
+		if freqSlice[i].Counter == freqSlice[j].Counter {
+			return freqSlice[i].Name < freqSlice[j].Name
+		}
+		return freqSlice[i].Counter > freqSlice[j].Counter
+	})
+
+	resultSlice := make([]string, 0)
+	for ind, value := range freqSlice {
+		if ind == 10 {
+			break
+		}
+		resultSlice = append(resultSlice, value.Name)
+	}
+	return resultSlice
 }
