15cb8167acebbcc106a98af89dc7c8e652b5d5ad
vtsapko <vtsapko@diasoft.ru>
2025-01-29 23:44:54 +0400

    hw7

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,67 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrFileInfo              = errors.New("failed to get source file info")
+	ErrLimit                 = errors.New("limit might not be lower then 0")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	fromFile, err := os.OpenFile(fromPath, os.O_RDONLY, 0)
+	if err != nil {
+		return fmt.Errorf("source file error: %w", err)
+	}
+	defer fromFile.Close()
+
+	fromInfo, err := fromFile.Stat()
+	if err != nil {
+		return ErrFileInfo
+	}
+
+	if !fromInfo.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+
+	if offset > fromInfo.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if limit < 0 {
+		return ErrLimit
+	}
+
+	toFile, err := os.Create(toPath)
+	if err != nil {
+		return fmt.Errorf("failed to create output file: %w", err)
+	}
+	defer toFile.Close()
+
+	if _, err := fromFile.Seek(offset, io.SeekStart); err != nil {
+		return fmt.Errorf("failed to seek input file: %w", err)
+	}
+
+	bytesToCopy := fromInfo.Size() - offset
+	if limit > 0 && limit < bytesToCopy {
+		bytesToCopy = limit
+	}
+
+	bar := pb.Full.Start64(bytesToCopy)
+	barReader := bar.NewProxyReader(fromFile)
+	defer bar.Finish()
+
+	_, err = io.CopyN(toFile, barReader, bytesToCopy)
+	if err != nil && !errors.Is(err, io.EOF) {
+		return fmt.Errorf("failed to copy data: %w", err)
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,71 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
 
-func TestCopy(t *testing.T) {
-	// Place your code here.
+	"github.com/stretchr/testify/require"
+)
+
+const output = "./testdata/output.txt"
+
+func cleanup() {
+	err := os.Remove(output)
+	if err != nil {
+		return
+	}
+}
+
+func TestCopy0_0(t *testing.T) {
+	defer cleanup()
+	err := Copy("testdata/input.txt", output, 0, 0)
+	fact, _ := os.ReadFile(output)
+	expected, _ := os.ReadFile("testdata/out_offset0_limit0.txt")
+	require.Equal(t, expected, fact)
+	require.Nil(t, err)
+}
+
+func TestCopy0_10(t *testing.T) {
+	defer cleanup()
+	err := Copy("testdata/input.txt", output, 0, 10)
+	fact, _ := os.ReadFile(output)
+	expected, _ := os.ReadFile("testdata/out_offset0_limit10.txt")
+	require.Equal(t, expected, fact)
+	require.Nil(t, err)
+}
+
+func TestCopy0_1000(t *testing.T) {
+	defer cleanup()
+	err := Copy("testdata/input.txt", output, 0, 1000)
+	fact, _ := os.ReadFile(output)
+	expected, _ := os.ReadFile("testdata/out_offset0_limit1000.txt")
+	require.Equal(t, expected, fact)
+	require.Nil(t, err)
+}
+
+func TestCopy0_10000(t *testing.T) {
+	defer cleanup()
+	err := Copy("testdata/input.txt", output, 0, 10000)
+	fact, _ := os.ReadFile(output)
+	expected, _ := os.ReadFile("testdata/out_offset0_limit10000.txt")
+	require.Equal(t, expected, fact)
+	require.Nil(t, err)
+}
+
+func TestCopy100_1000(t *testing.T) {
+	defer cleanup()
+	err := Copy("testdata/input.txt", output, 100, 1000)
+	fact, _ := os.ReadFile(output)
+	expected, _ := os.ReadFile("testdata/out_offset100_limit1000.txt")
+	require.Equal(t, expected, fact)
+	require.Nil(t, err)
+}
+
+func TestCopy6000_1000(t *testing.T) {
+	defer cleanup()
+	err := Copy("testdata/input.txt", output, 6000, 1000)
+	expected, _ := os.ReadFile("testdata/out_offset6000_limit1000.txt")
+	fact, _ := os.ReadFile(output)
+	require.Equal(t, expected, fact)
+	require.Nil(t, err)
 }
