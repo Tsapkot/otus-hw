c6bd584e1267c1a2e351e08939c06f2affe6c00f
vtsapko <vtsapko@diasoft.ru>
2025-02-02 14:36:39 +0400

    hw8 сырье без тестов

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,35 @@
 package main
 
+import (
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	for key, val := range env {
+		if val.NeedRemove {
+			err := os.Unsetenv(key)
+			if err != nil {
+				return 1
+			}
+			continue
+		}
+
+		err := os.Setenv(key, val.Value)
+		if err != nil {
+			return 1
+		}
+	}
+
+	command := exec.Command(cmd[0], cmd[1:]...)
+	command.Stdin = os.Stdin
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+
+	err := command.Run()
+	if err != nil {
+		return 1
+	}
+	return 0
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	if len(os.Args) < 2 {
+		fmt.Println("not enough arguments")
+		os.Exit(1)
+	}
+
+	env, err := ReadDir(os.Args[1])
+	if err != nil {
+		fmt.Errorf("Error reading environment directory %s: %w", os.Args[1], err)
+		os.Exit(1)
+	}
+
+	os.Exit(RunCmd(os.Args[2:], env))
 }
