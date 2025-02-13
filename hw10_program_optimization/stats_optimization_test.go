efdfee5ed466d98062e66eeaa1cc21736cfbefa9
vtsapko <vtsapko@diasoft.ru>
2025-02-13 16:17:12 +0400

    hw10 линтер

--- hw10_program_optimization/stats_optimization_test.go
+++ hw10_program_optimization/stats_optimization_test.go
@@ -18,7 +18,7 @@ const (
 	timeLimit = 300 * time.Millisecond
 )
 
-func saveTestResults(dir string, content string) (string, error) {
+func saveTestResults(dir string, content string) (string, error) { //nolint:unused
 	if err := os.MkdirAll(dir, os.ModePerm); err != nil {
 		return "", fmt.Errorf("failed to create directory: %w", err)
 	}
@@ -66,16 +66,16 @@ func TestGetDomainStat_Time_And_Memory(t *testing.T) {
 
 	result := testing.Benchmark(bench)
 	mem := result.MemBytes
-	//resultString := fmt.Sprintf(
-	//	"Time used: %s / %s\nMemory used: %dMb / %dMb\n",
-	//	result.T, timeLimit,
-	//	mem/mb, memoryLimit/mb,
-	//)
-	//filePath, err := saveTestResults("testresults", resultString)
-	//if err != nil {
-	//	t.Fatalf("Failed to save test results: %v", err)
-	//}
-	//t.Logf("Test results saved to: %s", filePath)
+	/*	resultString := fmt.Sprintf(
+			"Time used: %s / %s\nMemory used: %dMb / %dMb\n",
+			result.T, timeLimit,
+			mem/mb, memoryLimit/mb,
+		)
+		filePath, err := saveTestResults("testresults", resultString)
+		if err != nil {
+			t.Fatalf("Failed to save test results: %v", err)
+		}
+		t.Logf("Test results saved to: %s", filePath) */
 
 	t.Logf("time used: %s / %s", result.T, timeLimit)
 	t.Logf("memory used: %dMb / %dMb", mem/mb, memoryLimit/mb)
